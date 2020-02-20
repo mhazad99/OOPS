b95c3d436312cb9eb56ec31a9b09941895df7190
MHAZAD <mhazad1999@gmail.com>
2020-02-20 11:58:13 +0300

    new commitment. methodes are made

--- oop_proj.py
+++ oop_proj.py
@@ -1,5 +1,32 @@
-class Oop:
-    def __init__(self, a):
-        self.a = a
-        self.list = []    
+class Graph:
+    
+    def __init__(self):
+        self.Node = {}
+        self.Link = {}
+
+    def addNode(self, node):
+        pass
+
+    def delNode(self):
+        pass
+
+    def addLink(self):
+        pass
+
+    def delLink(self):
+        pass
+
+
+
+class Node(Graph):
+    def __init__(self, id, Location, Degrees):
+        self.id = id
+        self.Location = Location
+        self.Degrees = Degrees
+
+class Link(Graph) :
+    def __init__(self, InNodeId, OutNodeId):
+        self.InNodeId = InNodeId
+        self.OutNodeId = OutNodeId
+
     
