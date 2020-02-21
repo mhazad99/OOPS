23fff92a5c926d1602c5b2177ac7715eb7c3db65
MHAZAD <mhazad1999@gmail.com>
2020-02-21 17:02:33 +0300

    this is the last commit, you freeky:)))

--- oop_proj.py
+++ oop_proj.py
@@ -6,42 +6,62 @@ class Graph:
         self.link = []
         self.nodecounter = 0
 
+    class Node:
+        def __init__(self, Id, Location, Degrees):
+            self.Id = Id
+            self.Location = Location
+            self.Degrees = Degrees
+
+
+    class Link:
+        def __init__(self, InNodeId, OutNodeId):
+            self.InNodeId = InNodeId
+            self.OutNodeId = OutNodeId
+
+
     def addNode(self, newnode):
 
         self.node[self.nodecounter] = newnode
         self.nodecounter += 1
 
     def delNode(self, Id):
+        if self.node[Id]:
+            del self.node[Id]
+        else:
+            print('this node is not exist for deleting')
 
-        del self.node[self.nodecounter]
 
-    def addLink(self, innode, outnode):
-
+    def addLink(self, Link):
+        innode = Link.InNodeId
+        outnode = Link.OutNodeId
         self.link.append((innode, outnode))
 
 
     def delLink(self, innode, outnode):
-        
-        self.link.remove((innode, outnode))
+        if self.link.count((innode, outnode)) >= 1:
+            self.link.remove((innode, outnode))
+        else:
+            print('there is no such link to remove!!!')
 
+    def ShowOutPut(self):
 
-class Node:
-    def __init__(self, Id, Location, Degrees):
-        self.Id = Id
-        self.Location = Location
-        self.Degrees = Degrees
-
-class Link:
-    def __init__(self, InNodeId, OutNodeId):
-        self.InNodeId = InNodeId
-        self.OutNodeId = OutNodeId
+        print(self.node)
+        print(self.link)
 
 
 if __name__ == "__main__":
-    G = Graph
-    G.addNode(Node(0, 13, 4))
-    G.addNode(Node(1, 18, 3))
-    G.addLink(Link(0, 1))
+    G = Graph()
+    G.addNode(G.Node(0, 13, 4))
+    G.addNode(G.Node(1, 18, 3))
+    G.addNode(G.Node(2, 13, 4))
+    G.addNode(G.Node(3, 18, 3))
+    G.addLink(G.Link(0, 1))
+    G.addLink(G.Link(1, 3))
+    G.addLink(G.Link(2, 1))
+    G.delNode(1)
+    G.delLink(3, 1)
+    G.ShowOutPut()
+
 
 
 
